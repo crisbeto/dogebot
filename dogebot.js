ee6824f7a24cfb1a96b408b959f00d428cfb2088
crisbeto <crisbeto@abv.bg>
2017-03-14 21:52:30 +0100

    chore: indentation

--- dogebot.js
+++ dogebot.js
@@ -4,36 +4,36 @@ var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var doge = require('dogefy');
 
 var getTag = function(id){
-    return '<@' + id + '>';
+  return '<@' + id + '>';
 };
 
 var isDirect = function(userId, messageText){
-    return messageText && messageText.indexOf(userId) > -1;
+  return messageText && messageText.indexOf(userId) > -1;
 };
 
 module.exports = function(key){
-    if(!key) throw new Error('You need to specify a Slack token');
+  if(!key) throw new Error('You need to specify a Slack token');
 
-    var rtm = new RtmClient(key);
+  var rtm = new RtmClient(key);
 
-    rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
-        var team = rtm.dataStore.getTeamById(rtm.activeTeamId);
-        var user = rtm.dataStore.getUserById(rtm.activeUserId);
-        console.log('Connected to ' + team.name + ' as ' + user.name);
-    });
+  rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
+    var team = rtm.dataStore.getTeamById(rtm.activeTeamId);
+    var user = rtm.dataStore.getUserById(rtm.activeUserId);
+    console.log('Connected to ' + team.name + ' as ' + user.name);
+  });
 
-    rtm.on(RTM_EVENTS.MESSAGE, function(message){
-        var channel = rtm.dataStore.getChannelGroupOrDMById(message.channel);
+  rtm.on(RTM_EVENTS.MESSAGE, function(message){
+    var channel = rtm.dataStore.getChannelGroupOrDMById(message.channel);
 
-        if(message.type === 'message' && message.text && (channel.is_im || isDirect(rtm.activeUserId, message.text))){
-            var cleanedMessage = message.text.replace(getTag(rtm.activeUserId), '').replace(':', '').substring(0, 2000).trim();
+    if(message.type === 'message' && message.text && (channel.is_im || isDirect(rtm.activeUserId, message.text))){
+      var cleanedMessage = message.text.replace(getTag(rtm.activeUserId), '').replace(':', '').substring(0, 2000).trim();
 
-            if(cleanedMessage){
-                console.log('Dogefied: ' + cleanedMessage);
-                rtm.sendMessage('``` \n' + doge(cleanedMessage) + '\n ```', message.channel);
-            }
-        }
-    });
+      if(cleanedMessage){
+        console.log('Dogefied: ' + cleanedMessage);
+        rtm.sendMessage('``` \n' + doge(cleanedMessage) + '\n ```', message.channel);
+      }
+    }
+  });
 
-    rtm.start();
+  rtm.start();
 };
